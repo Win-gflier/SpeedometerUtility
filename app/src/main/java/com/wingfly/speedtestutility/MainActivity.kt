a2882bf4f181ffeb6703d2f9ac1aa7cf5d3b375a
Chirag Talsaniya <chirag.talsaniya@silvertouch.com>
2021-10-07 17:04:12 +0530

    Connection status

--- app/src/main/java/com/wingfly/speedtestutility/MainActivity.kt
+++ app/src/main/java/com/wingfly/speedtestutility/MainActivity.kt
@@ -4,21 +4,39 @@ import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.animation.doOnEnd
-import com.github.anastr.speedviewlib.PointerSpeedometer
+import java.lang.String
+
 
 class MainActivity : AppCompatActivity() {
+    var image: ImageView? = null
+    var progressBar: ProgressBar? = null
+    var status = 0
+    var awesomeSpeedometer: AwesomeSpeedometer? = null
+    var imageShadow: ImageView? = null
+    var txtConnect: TextView? = null
+    var shadowAnimatorSet: AnimatorSet? = null
+    var scaleDownAnimatorSet: AnimatorSet? = null
+    private val handler = Handler()
+    var scaleUpAnimatorSet: AnimatorSet? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        awesomeSpeedometer = findViewById<AwesomeSpeedometer>(R.id.speed)
+        image = findViewById<ImageView>(R.id.main_background)
+        txtConnect = findViewById<TextView>(R.id.txtConnect)
+        progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        imageShadow = findViewById<ImageView>(R.id.main_background2)
 
-        val findViewById = findViewById<AwesomeSpeedometer>(R.id.speed)
-        findViewById.speedTo(50F)
-        /*val image = findViewById<ImageView>(R.id.main_background)
-        val imageShadow = findViewById<ImageView>(R.id.main_background2)
+        progressBar?.visibility =View.VISIBLE
+
 
 
         val shadowAnim = ObjectAnimator.ofPropertyValuesHolder(
@@ -26,11 +44,11 @@ class MainActivity : AppCompatActivity() {
             PropertyValuesHolder.ofFloat("scaleY", 1.25f),
             PropertyValuesHolder.ofFloat("scaleX", 1.25f),
             PropertyValuesHolder.ofFloat("alpha", 0f),
-            )
+        )
 
-        val shadowAnimatorSet = AnimatorSet()
-        val scaleDownAnimatorSet = AnimatorSet()
-        val scaleUpAnimatorSet = AnimatorSet()
+         shadowAnimatorSet = AnimatorSet()
+         scaleDownAnimatorSet = AnimatorSet()
+         scaleUpAnimatorSet = AnimatorSet()
 
         val scaleDown =
             ObjectAnimator.ofPropertyValuesHolder(
@@ -47,7 +65,7 @@ class MainActivity : AppCompatActivity() {
             )
 
 
-        scaleDownAnimatorSet.apply {
+        scaleDownAnimatorSet?.apply {
             play(scaleDown)
             duration = 100
             start()
@@ -57,19 +75,55 @@ class MainActivity : AppCompatActivity() {
                     duration = 200
                     start()
                     doOnEnd {
-                        imageShadow.visibility = View.VISIBLE
+                        imageShadow?.visibility = View.VISIBLE
                         shadowAnimatorSet.apply {
                             play(shadowAnim)
                             duration = 1500
                             start()
                             doOnEnd {
-                                scaleDownAnimatorSet.startDelay = 1300
-                                scaleDownAnimatorSet.start()
+                                scaleDownAnimatorSet!!.startDelay = 1300
+                                scaleDownAnimatorSet!!.start()
                             }
                         }
                     }
                 }
             }
-        }*/
+        }
+
+
+    }
+
+    fun connectingView(view: android.view.View) {
+
+        shadowAnimatorSet?.end()
+        scaleUpAnimatorSet?.end()
+        scaleDownAnimatorSet?.end()
+        image?.visibility = View.GONE
+        imageShadow?.visibility = View.GONE
+
+        progressBar?.visibility=View.VISIBLE
+        txtConnect?.text= "Подключение"
+
+
+        Thread {
+            while (status < 100) {
+                status += 1
+                handler.post(Runnable {
+                    progressBar!!.progress = status
+                })
+                try {
+                    Thread.sleep(16)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }.start()
+        // Code to show the loader here
+        Handler(Looper.getMainLooper()).postDelayed({
+            txtConnect?.visibility = View.GONE
+            progressBar?.visibility=View.GONE
+            awesomeSpeedometer?.visibility = View.VISIBLE
+            awesomeSpeedometer?.speedTo(50F)
+        }, 3000)
     }
 }
